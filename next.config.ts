74b8e1a145dd665336fc64b8aab5f125c1272f21
渡邉剛弘 <watanabetakahiro@watanabetakehironoMacBook-Air.local>
2026-06-05 22:07:22 +0900

    next.config.tsを修正

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-// next.config.ts
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    // ビルド時の ESLint エラーを無視してデプロイを通します
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
